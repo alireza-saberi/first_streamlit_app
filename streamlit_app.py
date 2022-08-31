172af1835d0294d1f5a38c700fd15e7ec288fc22
Alireza Saberi <alireza-saberi@users.noreply.github.com>
2022-08-31 14:34:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,5 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 #streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
