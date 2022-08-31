c1b0ea33034d13ca3524f951ff236600853f2caa
Alireza Saberi <alireza-saberi@users.noreply.github.com>
2022-08-31 10:56:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,6 @@ import pandas
 
 #New part to see the response
 import requests
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
